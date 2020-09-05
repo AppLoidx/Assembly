aa8dffbde4823d0d4d793f8e8dc367068e0c332d
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-09-05 13:07:16 +0300

    add simple hello file

---
+++ basic/hello.asm
@@ -0,0 +1,18 @@
+section .data
+message:
+db  'hello, Onee-chan!', 12
+
+section .text
+global _start
+
+_start:
+    mov     rax, 1           ; 'write'
+    mov     rdi, 1           ;
+    mov     rsi, message     ;
+    mov     rdx, 20          ;
+    
+    syscall
+
+    mov     rax, 60          ; 'exit'
+    xor     rdi, rdi
+    syscall
