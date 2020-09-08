9d2f800a9fc3c4185fa1bff5ebed4f2231fd32ff
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-09-08 17:30:52 +0300

    build shell script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INF_C="\033[1;32m"  # green (light)
+EMP_C="\033[0m"     # no color
+DGR_C="\033[0;33m" # red
+
+for i in **/*.asm
+do
+        echo -e "${INF_C}[INFO]${EMP_C} Building file : $i"
+        # nasm compile
+        echo -e "${INF_C}[INFO]${EMP_C} nasm -felf64 $i -o ${i%.c}.o"
+        nasm "$i" -felf64 -o "${i%.c}.o"
+        
+        # linking
+        echo -e "${INF_C}[INFO]${EMP_C} ld ${i%.c}.o -o ${i%.c}.out"
+        ld "${i%.c}.o" -o "${i%.c}".out
+        done
+
+echo -e "${DGR_C}[DELETE]${EMP_C} Deleting temp files..."
+find . -type f -name '*.o' -delete
+echo -e "${INF_C}DONE!${EMP_C}"
